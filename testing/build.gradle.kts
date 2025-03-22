44ad26adae8cc0d0a45e2f5a531c54ea570b1665
Rohit Verma <rv17837@outlook.com>
2025-03-22 19:36:30 +0530

    test(testing): convert testing module to kmp module

--- testing/build.gradle.kts
+++ testing/build.gradle.kts
@@ -1,16 +1,29 @@
+/*
+ * Copyright 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/kmp-project-template/blob/main/LICENSE
+ */
 plugins {
-    alias(libs.plugins.android.library.convention)
+    alias(libs.plugins.kmp.library.convention)
 }
 
 android {
     namespace = "org.mifos.testing"
 }
 
-dependencies {
-    api(libs.kotlinx.coroutines.test)
-    api(projects.core.common)
-    api(libs.kotlin.test)
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            api(libs.kotlinx.coroutines.test)
+            api(projects.core.common)
+            api(libs.kotlin.test)
 
-    implementation(libs.koin.core)
-    implementation(libs.koin.test)
+            implementation(libs.koin.core)
+            implementation(libs.koin.test)
+        }
+    }
 }
--- testing/src/commonMain/kotlin/org/mifos/testing/di/TestDispatcherModule.kt
+++ testing/src/commonMain/kotlin/org/mifos/testing/di/TestDispatcherModule.kt
@@ -1,6 +1,16 @@
+/*
+ * Copyright 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See See https://github.com/openMF/kmp-project-template/blob/main/LICENSE
+ */
 package org.mifos.testing.di
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
@@ -9,6 +19,7 @@ import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.mifos.core.common.di.AppDispatchers
 
+@OptIn(ExperimentalCoroutinesApi::class)
 val TestDispatchersModule: Module = module {
     single<TestDispatcher> { UnconfinedTestDispatcher() }
     single<CoroutineDispatcher>(named(AppDispatchers.IO.name)) { StandardTestDispatcher() }
